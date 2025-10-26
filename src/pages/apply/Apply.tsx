81dc5737a6f9509e0fe338653e874a319d0a9861
yoojanghun <yoojanghun@naver.com>
2025-10-26 13:42:11 +0900

    전화번호 추가

--- src/pages/apply/Apply.tsx
+++ src/pages/apply/Apply.tsx
@@ -4,27 +4,67 @@ import {
   TextField, 
   Button, 
   Box,
+  FormControl,
+  Input,
+  InputLabel,
   ThemeProvider } from "@mui/material";
+import { IMaskInput } from "react-imask";
 import inputTheme from "../../styles/inputTheme.ts";
 import btnTheme from "../../styles/btnTheme.ts";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import React from "react";
 
+interface CustomProps {
+  onChange: (event: { target: { name: string; value: string } }) => void;
+  name: string;
+}
+
+const TextMaskCustom = React.forwardRef<HTMLInputElement, CustomProps>(
+  function TextMaskCustom(props, ref) {
+    const { onChange, ...other } = props;
+    return (
+      <IMaskInput
+        {...other} 
+        mask="000-0000-0000"
+        definitions={{
+          '#': /[0-9]/,
+        }}
+        inputRef={ref}
+        onAccept={(value: any) => onChange({ target: { name: props.name, value } })}
+        overwrite
+      />
+    );
+  },
+);
+
+{/* useState는 다음 렌더링 때, 특정 값으로 바꿔달라고 요청만 하는 것 (useState의 비동기 특성) */}
 function Apply() {
   const navigate = useNavigate();
   const [applyPage, setApplyPage] = useState(1);
   const [username, setUsername] = useState("");
+  const [userage, setUserage] = useState("");
   const [error, setError] = useState(true);
+  const [phoneNum, setPhoneNum] = useState({
+    textmask: '',
+    numberformat: '1320',
+  });
 
-  function handleName(e: React.ChangeEvent<HTMLInputElement>) {
-    if (e.target.value.trim() === "") {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNum({
+      ...phoneNum,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (username.trim() === "" || userage === "" || phoneNum.textmask === "") {
       setError(true);
     }
     else {
       setError(false);
-      setUsername(e.target.value);
     }
-  }
+  }, [username, userage, phoneNum.textmask])
 
   return(
     <>
@@ -40,14 +80,37 @@ function Apply() {
                   label="성함" 
                   variant="standard"
                   required
-                  onChange={handleName}
                   value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
-                <TextField 
+                <TextField
+                  type="number"
                   label="나이"
+                  variant="standard" 
                   required
-                  variant="standard"  
+                  value={userage}
+                  onChange={(e) => setUserage(e.target.value)}
+                  slotProps={{
+                    input: {
+                      inputProps: {
+                        min: 0,
+                        max: 100,
+                        step: 1
+                      }
+                    }
+                  }}
                 />
+                <FormControl variant="standard">
+                  <InputLabel htmlFor="formatted-text-mask-input">전화번호</InputLabel>
+                  <Input
+                    value={phoneNum.textmask}
+                    onChange={handleChange}
+                    required
+                    name="textmask"
+                    id="formatted-text-mask-input"
+                    inputComponent={TextMaskCustom as any}
+                  />
+                </FormControl>
               </ThemeProvider>
               <ThemeProvider theme={btnTheme}>
                 <Button variant="contained" 
@@ -87,6 +150,10 @@ function Apply() {
         {applyPage === 3 && (
           <div>
             <h2>질문 있으면 적어주세요</h2>
+            <TextField 
+              multiline
+              rows={5}
+            />
             <ThemeProvider theme={btnTheme}>
               <Button variant="contained" 
                 onClick={() => setApplyPage(applyPage - 1)}
