d57395b0c41adde80b5306bc2a436fc0bf3d49af
yoojanghun <yoojanghun@naver.com>
2025-10-27 23:41:34 +0900

    home page 배경 이미지

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Header from "../../shared/header/Header.tsx";
 import { Button, ThemeProvider } from "@mui/material";
 import btnTheme from "../../styles/btnTheme.ts";
 import { useNavigate } from "react-router-dom";
+import backgroundPiano from "../../assets/piano-bg.jpg";
 
 function Home() {
   const navigate = useNavigate();
@@ -9,7 +10,10 @@ function Home() {
   return (
     <>
       <Header />
-      <main className="min-h-screen pt-15" >
+      <main className="min-h-screen pt-12" >
+        <section className="w-screen h-screen overflow-hidden">
+          <img src={backgroundPiano} className="w-full h-full object-cover object-[center_70%]" alt="piano-background-pic" />
+        </section>
         <h1 className="bg-red-400">이수정 피아니스트 사이트</h1>
         <ThemeProvider theme={btnTheme}>
           <Button variant="contained" onClick={() => navigate("/apply")}>
